589116c93b34380ed0c7e3e4bb4f72bd51e7c78c
Smayan Nirantare <smayannirantare5@gmail.com>
2024-10-12 16:03:47 -0700

    openai

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import { TextInput } from "./components/text-input";
 import logo from "./logo.svg";
+import { getChatCompletion } from "./scripts/openai";
 
 function App() {
+	const handleSubmit = async (text) => {
+		console.log("SUBMIT");
+		const stream = await getChatCompletion(text);
+
+		for (const chunk in stream) {
+			console.log(chunk);
+		}
+	};
+
 	return (
 		<div className="App">
 			<header className="App-header">
-				<TextInput />
+				<TextInput handleSubmit={handleSubmit} />
 			</header>
 		</div>
 	);
