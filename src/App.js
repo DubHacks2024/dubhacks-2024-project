f133c3aa56092a2d307d7a693dba5560c53748e2
Smayan Nirantare <smayannirantare5@gmail.com>
2024-10-12 16:16:04 -0700

    openai connection works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import { TextInput } from "./components/text-input";
 import logo from "./logo.svg";
 import { getChatCompletion } from "./scripts/openai";
 
 function App() {
+	const [streamText, setStreamText] = useState("");
+
 	const handleSubmit = async (text) => {
 		console.log("SUBMIT");
 		const stream = await getChatCompletion(text);
 
-		for (const chunk in stream) {
-			console.log(chunk);
+		let runningStream = "";
+		for await (const part of stream) {
+			const chunk = part.choices[0]?.delta?.content || "";
+			runningStream += chunk;
+			setStreamText(runningStream);
 		}
 	};
 
@@ -17,6 +23,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<TextInput handleSubmit={handleSubmit} />
+				<div>{streamText}</div>
 			</header>
 		</div>
 	);
