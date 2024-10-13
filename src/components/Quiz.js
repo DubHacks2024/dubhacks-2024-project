eb44e70a8e786e7a67a54cd33bd515c4be6d010e
Smayan Nirantare <smayannirantare5@gmail.com>
2024-10-12 21:00:57 -0700

    quiz component working

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,35 +1,12 @@
 import React, { Component } from "react";
 import MultiChoice from "./MultiChoice";
 
-// export class Quiz extends Component {
-// 	constructor(props) {
-// 		super(props);
-// 		// for (let i = 0; i < quiz.length; i++) {
-// 		// 	quizList.push(JSON.parse(quiz[i]));
-// 		// }
-// 		this.state = { quizList: props.quiz };
-// 	}
-
-// 	render = () => {
-// 		return (
-// 			<div>
-// 				<ol>
-// 					{this.state.quizList.map((cur) => {
-// 						return MultiChoice(cur);
-// 					})}
-// 					;
-// 				</ol>
-// 			</div>
-// 		);
-// 	};
-// }
-
 function Quiz({ quiz }) {
 	return (
 		<div>
 			<ol>
-				{quiz.map((cur) => {
-					return <MultiChoice problem={cur} />;
+				{quiz.map((cur, idx) => {
+					return <MultiChoice problem={cur} key={idx} />;
 				})}
 			</ol>
 		</div>
