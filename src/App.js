a2e33289337d3e2c0ca81be5f960108301f7017e
Smayan Nirantare <smayannirantare5@gmail.com>
2024-10-12 22:07:33 -0700

    small change

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
 	const [quiz, setQuiz] = useState([]);
 
 	const handleSubmit = async (text, type) => {
+		setActive(type);
+
 		if (type === "summary") {
 			const stream = await getSummary(text);
 			let runningStream = "";
@@ -56,22 +58,6 @@ function App() {
 		} else if (active === "discussion") {
 			return (
 				<article>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
-					<h1>DISCUSSION</h1>
 					<h1>DISCUSSION</h1>
 				</article>
 			);
